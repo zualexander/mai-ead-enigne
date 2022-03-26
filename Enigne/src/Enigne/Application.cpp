1b7ced3050ea3f2ded4d9efc81c1e096cd792595
Alexander Zulechner <alexanderzulechner@gmail.com>
2022-03-27 00:26:21 +0100

    color window

--- Enigne/src/Enigne/Application.cpp
+++ Enigne/src/Enigne/Application.cpp
@@ -11,6 +11,8 @@ namespace Enigne {
 
     void Application::Run() {
         while (isRunning) {
+            glClearColor(1, 0, 1, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
             window->onUpdate();
         }
     }
